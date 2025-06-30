672de8301cf1962af5f0bdb65d5f3ed8ef1cb256
Ali Azimo <webdesign.aejl@gmail.com>
2025-06-30 14:23:13 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+    //Hero Silde
+        $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        nav:true,
+        items: 1,
+        dots: false,
+        responsive:{
+            0:{
+               
+            },
+            700:{
+               
+            }
+        
+        }
+    });
+    //More project
+        $('#project').owlCarousel({
+        loop:true,
+        margin:0,
+        items: 1,
+        smartSpeed: 1000,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        nav:true,
+        dots: false,
+        responsive:{
+            0:{
+               
+            },
+            700:{
+               
+            },
+            1400:{
+
+            }
+        }
+    })
+    //Detail
+        $('#detail').owlCarousel({
+        loop:true,
+        margin:0,
+        items: 1,
+        autoplay: false,
+        nav:true,
+        dots: false,
+        responsive:{
+            0:{
+               
+            },
+            700:{
+               
+            },
+            1400:{
+
+            }
+        }
+    })
+
+});
